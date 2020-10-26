ced6051ff3f11a35cfab85447a20fb0f0e70d838
amlan95 <64047246+amlan95@users.noreply.github.com>
2020-10-26 23:01:29 +0530

    Update hr-diagram.py

--- hr-diagram.py
+++ hr-diagram.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from matplotlib.cm import get_cmap
-from matplotlib.colors import PowerNorm
+from matplotlib.colors import LogNorm
 cmap = get_cmap('inferno')
 
-data = pd.read_csv('1603547891155O-result.csv')
+data = pd.read_csv('1603729801595O-result.csv')
 bp_rp = data['bp_rp']
 mg = data['mg']
 
@@ -19,11 +19,11 @@ ax.scatter(bp_rp, mg, alpha=0.5, s=10, color='red', zorder=0)
 plt.gca().invert_yaxis()
 
 cb = fig.colorbar(h[3], ax=ax, pad=0.02)
-plt.title('Colour Magnitude Diagram of 500000 stars' , fontsize=18)
+plt.title('Colour Magnitude Diagram of 900000 stars' , fontsize=18)
 plt.xlabel(r'$G_{BP} - G_{RP}$' , fontsize=14)
 plt.ylabel(r'$M_G$' , fontsize=14)
 cb.set_label(r"$\mathrm{Stellar~density}$" , fontsize=14)           
 
-plt.savefig("hr-diagram.jpg")
+plt.savefig("hr-diagram.jpg", dpi=400)
 
 plt.show()
