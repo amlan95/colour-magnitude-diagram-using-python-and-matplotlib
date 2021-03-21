710b845f4344ae2d7ae29ff2e3d7cceb1293d5f1
amlan95 <64047246+amlan95@users.noreply.github.com>
2021-03-21 15:28:45 +0530

    Update hr-diagram.py

--- hr-diagram.py
+++ hr-diagram.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 from matplotlib.cm import get_cmap
 from matplotlib.colors import LogNorm
-cmap = get_cmap('inferno')
+
 
 data = pd.read_csv('1603729801595O-result.csv')
 bp_rp = data['bp_rp']
 mg = data['mg']
 
 fig, ax = plt.subplots(figsize=(12, 12))
-h=ax.hist2d(bp_rp, mg, bins = 300,  
-          norm = colors.LogNorm(), 
-          cmap ="Reds") 
-ax.scatter(bp_rp, mg, alpha=0.5, s=10, color='red', zorder=0)
+h=ax.hist2d(bp_rp, mg, bins = 200, cmin= 10, 
+          norm = colors.LogNorm(), zorder=0.5,
+          cmap ="gist_heat") 
+ax.scatter(bp_rp, mg, alpha=0.2, s=5, color='Black', zorder=0)
 
 plt.gca().invert_yaxis()
 
@@ -24,6 +24,6 @@ plt.xlabel(r'$G_{BP} - G_{RP}$' , fontsize=14)
 plt.ylabel(r'$M_G$' , fontsize=14)
 cb.set_label(r"$\mathrm{Stellar~density}$" , fontsize=14)           
 
-plt.savefig("hr-diagram.jpg", dpi=400)
+plt.savefig("hr-diagram.png", dpi=400)
 
 plt.show()
